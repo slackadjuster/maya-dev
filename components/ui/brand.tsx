db1ee7dc7f17fc0b7a5b9e7585abec701120fda3
Slacky <149196684+slackadjuster@users.noreply.github.com>
2024-04-25 04:09:57 +0800

    Update brand.tsx

--- components/ui/brand.tsx
+++ components/ui/brand.tsx
@@ -12,15 +12,14 @@ export const Brand: FC<BrandProps> = ({ theme = "dark" }) => {
   return (
     <Link
       className="flex cursor-pointer flex-col items-center hover:opacity-50"
-      href="https://www.mysati.org"
+      href=""
       target="_blank"
       rel="noopener noreferrer"
     >
-      <div className="mb-2">
-        <ChatbotUISVG theme={theme === "dark" ? "dark" : "light"} scale={0.3} />
+      <div className="mb-1">
+        <ChatbotUISVG theme={theme === "dark" ? "dark" : "light"} scale={0.9} />
       </div>
 
-      <div className="text-4xl font-bold tracking-wide">MayaGPT</div>
     </Link>
   )
 }
